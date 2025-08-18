3bd5bbc79817ed7c83de68dd141228b95c4aeeb9
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2025-08-18 17:05:07 +0200

    php works

--- sse/events.php
+++ sse/events.php
@@ -1,11 +1,13 @@
 <?php
-header( "Content-type: text/event-stream" );
-header( "Cache-Control: no-cache" );
+set_time_limit(0);
+header("Content-Type: text/event-stream");
+header("Cache-Control: no-cache");
+header("Connection: keep-alive");
 ob_end_flush();
 
 while( true ){
-    echo "event: message \n";
-    echo "data: dadada \n\n";
+    echo "event: message\n";
+    echo "data: " . date("H:i:s") . "\n\n";
     flush(); //Flush partial messages 
 
     if( connection_aborted ()) break; //if the user eliminates the web, stop sending data 
